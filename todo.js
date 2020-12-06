4cda43678503e14b2623e16eed5e385685ddc1f7
sowjd <sowjd1225@gmail.com>
2020-12-07 00:31:41 +0900

    Add todo and done

--- todo.js
+++ todo.js
@@ -1,34 +1,68 @@
 const todoForm = document.querySelector(".js-todo");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.querySelector(".js-todoList");
+const doneList = document.querySelector(".js-doneList");
 
 const TODO_LS = "todo";
+const DONE_LS = "done";
 let todoArr = [];
+let doneArr = [];
 
-function deleteTodo(event){
+function saveTask(type) {
+    if (type === TODO_LS) {
+        localStorage.setItem(TODO_LS, JSON.stringify(todoArr));
+    } else {
+        localStorage.setItem(DONE_LS, JSON.stringify(doneArr));
+    }
+}
+
+function deleteFromTodo(event) {
     const li = event.target.parentNode;
     todoList.removeChild(li);
-    const deleteFromTodoArr = todoArr.filter(function(todo){
+    const deleteFromTodoArr = todoArr.filter(function (todo) {
         return todo.id !== parseInt(li.id);
     });
     todoArr = deleteFromTodoArr;
-    saveTodo();
+    saveTask(TODO_LS);
 }
 
-function saveTodo(){
-    localStorage.setItem(TODO_LS, JSON.stringify(todoArr));
+function deleteFromDone(event) {
+    const li = event.target.parentNode;
+    doneList.removeChild(li);
+    const deleteFromDoneArr = doneArr.filter(function (done) {
+        return done.id !== parseInt(li.id);
+    });
+    doneArr = deleteFromDoneArr;
+    saveTask(DONE_LS);
 }
 
-function showTodo(text) {
+function moveToDone(event) {
+    const li = event.target.parentNode;
+    todoArr.forEach(function (todo) {
+        if (todo.id === parseInt(li.id)) {
+            showDoneList(todo.text);
+        }
+    });
+    deleteFromTodo(event);
+}
+
+function showTodoList(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
+    delBtn.innerHTML = "✕";
+    delBtn.addEventListener("click", deleteFromTodo);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.innerHTML = "✓";
+    doneBtn.addEventListener("click", moveToDone);
+
     const span = document.createElement("span");
     const newId = todoArr.length + 1;
-    delBtn.innerHTML = "❌";
-    delBtn.addEventListener("click", deleteTodo);
-    span.innerText = text;
+
+    span.innerText = text + "   ";
     li.appendChild(span);
     li.appendChild(delBtn);
+    li.appendChild(doneBtn);
     li.id = newId;
     todoList.appendChild(li);
 
@@ -37,31 +71,76 @@ function showTodo(text) {
         id: newId
     };
     todoArr.push(todoObj);
-    saveTodo();
+    saveTask(TODO_LS);
 }
 
-function handleSubmit() {
+function moveToTodo(event) {
+    const li = event.target.parentNode;
+    doneArr.forEach(function (done) {
+        if (done.id === parseInt(li.id)) {
+            showTodoList(done.text);
+        }
+    });
+    deleteFromDone(event);
+}
+
+function showDoneList(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    delBtn.innerHTML = "✕";
+    delBtn.addEventListener("click", deleteFromDone);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.innerHTML = "<<";
+    doneBtn.addEventListener("click", moveToTodo);
+
+    const span = document.createElement("span");
+    const newId = todoArr.length + 1;
+
+    span.innerText = text + "   ";
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    li.appendChild(doneBtn);
+    li.id = newId;
+    doneList.appendChild(li);
+
+    const todoObj = {
+        text: text,
+        id: newId
+    };
+    doneArr.push(todoObj);
+    saveTask(DONE_LS);
+}
+
+
+function addTodo(event) {
     event.preventDefault();
     const currentValue = todoInput.value;
-    showTodo(currentValue);
+    showTodoList(currentValue);
     todoInput.value = "";
 }
 
-function loadTodo() {
+function loadTask() {
     const todos = localStorage.getItem(TODO_LS);
-
     if (todos !== null) {
         const parseTodo = JSON.parse(todos);
-        parseTodo.forEach(function(todo){
-            showTodo(todo.text);
+        parseTodo.forEach(function (todo) {
+            showTodoList(todo.text);
         });
+    }
 
+    const dones = localStorage.getItem(DONE_LS);
+    if (dones !== null) {
+        const parseDone = JSON.parse(dones);
+        parseDone.forEach(function (done) {
+            showDoneList(done.text);
+        });
     }
 }
 
 function init() {
-    loadTodo();
-    todoForm.addEventListener("submit", handleSubmit);
+    loadTask();
+    todoForm.addEventListener("submit", addTodo);
 }
 
 init();
